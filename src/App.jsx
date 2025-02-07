ee5431d8cc0e1b02d232622068c10f89ac6034fb
J_dev <jss.seeingred@gmail.com>
2025-02-07 20:20:26 +0100

    modificacion de stilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import Services from './components/Services';
-import AboutUs from './components/AboutUs';
-import Porfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import AboutUs from "./components/AboutUs";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle('dark-mode', !darkMode); // Aplicar clase al body
-  };
+  useEffect(() => {
+    document.body.className = darkMode ? "dark-mode" : "light-mode";
+  }, [darkMode]);
 
   return (
-    <div className={darkMode ? "dark" : "light"}>
-      <Navbar toggleDarkMode={toggleTheme} isDarkMode={darkMode} />
+    <div>
+      <Navbar toggleDarkMode={() => setDarkMode(!darkMode)} isDarkMode={darkMode} />
       <HeroSection isDarkMode={darkMode} />
       <Services isDarkMode={darkMode} />
       <AboutUs isDarkMode={darkMode} />
-      <Porfolio isDarkMode={darkMode} />
+      <Portfolio isDarkMode={darkMode} />
       <Contact isDarkMode={darkMode} />
       <Footer isDarkMode={darkMode} />
     </div>
